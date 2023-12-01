7bddb538677210ed7444eed0d314831de10200f0
Anton Beck <anto2238@gmail.com>
2023-12-01 15:57:59 +0100

    bab

--- afe_fit/src/app/client/page.tsx
+++ afe_fit/src/app/client/page.tsx
@@ -1,50 +1,49 @@
-'use client'
-import React from 'react';
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import WorkoutProgram from '../Models/WorkoutProgram';
 import ProgramList from '../components/WorkoutProgramList';
-import jwt from 'jsonwebtoken';
-import Navbar from '../components/navbar';
+import jwt, { Jwt } from 'jsonwebtoken';
 
 
 const ClientPage = () => {
     const [workoutPrograms, setWorkoutPrograms] = useState<WorkoutProgram[]>([]);
-    const [jwtToken, setJwtToken] = useState<string | null>(null);
-    const [tokenDecoded, setTokenDecoded] = useState();
+    const [jwtToken, setJwtToken] = useState<string | null>('');
+    const [tokenDecoded, setTokenDecoded] = useState<Jwt | null>(null);
 
-    useEffect( () => {
+    useEffect(() => {
         const token = localStorage.getItem('jwtToken');
         setJwtToken(token);
-        setTokenDecoded(jwt.decode(token));      
+        setTokenDecoded(token ? (jwt.decode(token) as Jwt) : null);
     }, []);
 
     useEffect(() => {
-        if(tokenDecoded !== undefined){
-            var token = localStorage.getItem('jwtToken');
+        if (tokenDecoded !== null) {
+            const token = localStorage.getItem('jwtToken');
 
             fetch(`https://afefitness2023.azurewebsites.net/api/WorkoutPrograms/client/${tokenDecoded.UserId}`, {
-              method: 'GET',
-              headers: {
-                Authorization: `Bearer ${token}`,
-                'Content-Type': 'application/json',
-              },
-            }).then((response) => {
-              if (!response.ok) {
-                throw new Error(`Network response was not ok. Status: ${response.status}`);
-              }
-              return response.json();
+                method: 'GET',
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                    'Content-Type': 'application/json',
+                },
             })
-            .then((data: WorkoutProgram[]) => {
-              setWorkoutPrograms(data);           
-          }).catch((error) => console.error('Error fetching workout programs:', error.message));
+                .then((response) => {
+                    if (!response.ok) {
+                        throw new Error(`Network response was not ok. Status: ${response.status}`);
+                    }
+                    return response.json();
+                })
+                .then((data: WorkoutProgram[]) => {
+                    setWorkoutPrograms(data);
+                })
+                .catch((error) => console.error('Error fetching workout programs:', error.message));
         }
-    }, [tokenDecoded])
+    }, [tokenDecoded]);
 
     return (
-      <div style={{ maxWidth: '600px', margin: 'auto', padding: '20px' }}>
-        <Navbar></Navbar>
-        <ProgramList programs={workoutPrograms}/>
-      </div>
+        <div style={{ maxWidth: '600px', margin: 'auto', padding: '20px' }}>
+            {/* You can add Navbar here if needed */}
+            <ProgramList programs={workoutPrograms} />
+        </div>
     );
   };
   
