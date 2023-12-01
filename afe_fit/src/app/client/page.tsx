6d2542b6c1fc478adc90a856f8a2ad8d1cde3439
au683782 <84569264+odennis2@users.noreply.github.com>
2023-12-01 16:35:57 +0100

    fixed

--- afe_fit/src/app/client/page.tsx
+++ afe_fit/src/app/client/page.tsx
@@ -15,39 +15,34 @@ const ClientPage = () => {
   
     if (token !== null) {
       setJwtToken(token);
-    }
-    if (token) {
       try {
         const tokenDecoded = jwt.decode(token) as JwtPayload;
         setTokenDecoded(tokenDecoded);
+  
+        if (tokenDecoded !== null) {
+          fetch(`https://afefitness2023.azurewebsites.net/api/WorkoutPrograms/client/${tokenDecoded.UserId}`, {
+            method: 'GET',
+            headers: {
+              Authorization: `Bearer ${token}`,
+              'Content-Type': 'application/json',
+            },
+          })
+            .then((response) => {
+              if (!response.ok) {
+                throw new Error(`Network response was not ok. Status: ${response.status}`);
+              }
+              return response.json();
+            })
+            .then((data: WorkoutProgram[]) => {
+              setWorkoutPrograms(data);
+            })
+            .catch((error) => console.error('Error fetching workout programs:', error.message));
+        }
       } catch (error) {
-        console.error('Error decoding JWT token:');
+        console.error('Error decoding JWT token:', error);
       }
     }
-  })
-    useEffect(() => {
-        if (tokenDecoded !== null) {
-            const token = localStorage.getItem('jwtToken');
-
-            fetch(`https://afefitness2023.azurewebsites.net/api/WorkoutPrograms/client/${tokenDecoded.UserId}`, {
-                method: 'GET',
-                headers: {
-                    Authorization: `Bearer ${token}`,
-                    'Content-Type': 'application/json',
-                },
-            })
-                .then((response) => {
-                    if (!response.ok) {
-                        throw new Error(`Network response was not ok. Status: ${response.status}`);
-                    }
-                    return response.json();
-                })
-                .then((data: WorkoutProgram[]) => {
-                    setWorkoutPrograms(data);
-                })
-                .catch((error) => console.error('Error fetching workout programs:', error.message));
-        }
-    }, [tokenDecoded]);
+  }, []);
 
     return (
         <div style={{ maxWidth: '600px', margin: 'auto', padding: '20px' }}>
