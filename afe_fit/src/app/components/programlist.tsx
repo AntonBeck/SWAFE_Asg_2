96a63de0741076ca796713a721df89d999d62a4f
Rasmus-Engelund-Skole <202007668@post.au.dk>
2023-12-01 10:46:50 +0100

    models updated

--- afe_fit/src/app/components/programlist.tsx
+++ afe_fit/src/app/components/programlist.tsx
@@ -1,18 +1,27 @@
+'use client'
 // components/ProgramList.tsx
-import React from 'react';
+import { useState, useEffect } from 'react';
+import WorkoutProgram from '../Models/WorkoutProgram';
+import WorkoutProgramComponent from './WorkoutProgramComponent';
 
-const ProgramList: React.FC<{
-  programs: WorkoutProgram[];
-}> = ({ programs }) => {
+const ProgramList: React.FC<{programs: WorkoutProgram[]}> = ({ programs }) => {
+  const [selectedProgramId, setSelectedWorkoutProgramId] = useState<number | null>(null);
+
+  const handleProgramSelection = (programId: number) => {
+    setSelectedWorkoutProgramId(programId);
+  };
+
   return (
     <div>
       <h2>Workout Programs</h2>
       <ul>
-        {programs.map((program) => (
-          <li key={program.id}>
-            <a href={`/personal-trainer/${program.clientId}/${program.id}`}>{program.name}</a>
-          </li>
-        ))}
+        {/* {programs.map((program) => (
+          
+          // <WorkoutProgramComponent key={program.workoutProgramId}
+          //                          program={program}
+          //                          IsSelected={program.workoutProgramId === selectedProgramId}
+          //                          onClick={() => handleProgramSelection(program.workoutProgramId)}/>
+        ))} */}
       </ul>
     </div>
   );
